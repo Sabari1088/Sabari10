ed197570562df1025cc202b98950303789af9968
Sabari1088 <sabarinathan10@gmail.com>
2025-07-08 10:51:17 +0530

    Update GitClass.java

--- GitClass.java
+++ GitClass.java
@@ -6,7 +6,11 @@ public static void main(String[] args) {
 		
 		int a = 10;
 		int b = 15;
-		int c = a+b;
+		int c = 5;
+		int d = a+b+c;
+		System.out.println(d);
+		System.out.println(a);
+		System.out.println(b);
 		System.out.println(c);
 
 }	
